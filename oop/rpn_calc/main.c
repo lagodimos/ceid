0630db97ac48d69d31db89f811e9ed08885b09fc
Giannis Lagodimos <62063308+giann1s@users.noreply.github.com>
2024-02-28 13:20:58 +0200

    RPNCalc: Don't declare stack globally

--- oop/rpn_calc/main.c
+++ oop/rpn_calc/main.c
@@ -9,58 +9,48 @@ typedef struct stack {
 	int stack[STACK_SIZE];
 } Stack;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int getOp(char op[]);
-void putInStack(char s[]); // char *s
+void putInStack(Stack *st, char s[]);
 int getFromStack();
-void add();
-void sub();
-void mul();
-void divi();
-void presentResult();
-void push(int n);
-int pop();
-
-//char operand[20];
-//int inputType;
-
-//int stack[50];
-//int sp=0;
+void add(Stack *st);
+void sub(Stack *st);
+void mul(Stack *st);
+void divi(Stack *st);
+void presentResult(Stack *st);
+void push(Stack *st, int n);
+int pop(Stack *st);
 
 int main(int argc, char *argv[]) {
 	char operand[20];
 	int inputType;
 
+	Stack st = {0};
+
 	printf("RPN Calculator\n");
-//	push(10);
-//	push(3);
-//	printf("pop=%d\n",pop());
-//	printf("pop=%d\n",pop());
+
 	while((inputType=getOp(operand)) != EOF) {
-//		printf("inputType= %c(as char)\t %d(as decimal)\n",inputType,inputType);
-//		printf("operand=%s\n",operand);
+
 		switch(inputType) {
 			case OPERAND:
-				putInStack(operand);
+				putInStack(&st, operand);
 				break;
 			case '+':
-				add();
+				add(&st);
 				break;
 			case '-':
-				sub();
+				sub(&st);
 				break;
 			case '*':
-				mul();
+				mul(&st);
 				break;
 			case '/':
-				divi();
+				divi(&st);
 				break;
 			case '=':
-				presentResult();
+				presentResult(&st);
 				break;
 			default:
 				printf("operation not suported\n");
-
 		}
 	}
 
@@ -69,7 +59,7 @@ int main(int argc, char *argv[]) {
 
 int getOp(char op[]) {
 	char ch;
-//	printf("Eneter RPN expression\n");
+
 	ch=getchar();
 	while(ch==' '|| ch=='\n')
 		ch=getchar();
@@ -83,57 +73,53 @@ int getOp(char op[]) {
 }
 
 
-void putInStack(char s[]) {
-//	printf("putInStack\n");
-	push(atoi(s));
+void putInStack(Stack *st, char s[]) {
+	push(st, atoi(s));
 }
-
 
 int getFromStack() {
 
 }
 
-void add() {
-	push(pop() + pop());
+void add(Stack *st) {
+	push(st, pop(st) + pop(st));
 }
 
-void sub() {
-	int n = pop();
-	push(pop() - n);
+void sub(Stack *st) {
+	int n = pop(st);
+	push(st, pop(st) - n);
 }
 
-void mul() {
-	push(pop() * pop());
+void mul(Stack *st) {
+	push(st, pop(st) * pop(st));
 }
 
-void divi() {
-	int divisor = pop();
+void divi(Stack *st) {
+	int divisor = pop(st);
 
 	if (divisor != 0)
-		push(pop() / divisor);
+		push(st, pop(st) / divisor);
 	else {
-		push(divisor);
+		push(st, divisor);
 		printf("Division by 0! Operation ignored.");
 	}
 }
 
-void presentResult() {
-	printf("result=%d\n",pop());
+void presentResult(Stack *st) {
+	printf("result=%d\n",pop(st));
 }
 
-Stack st1 = {0};
+void push(Stack *st, int n) {
 
-void push(int n) {
-
-	if(st1.sp>=STACK_SIZE)
+	if(st->sp>=STACK_SIZE)
 		printf("Stack full\n");
 	else
-		st1.stack[st1.sp++]=n;
+		st->stack[st->sp++]=n;
 }
 
-int pop() {
-	if(st1.sp==0)
+int pop(Stack *st) {
+	if(st->sp==0)
 		printf("stack empty\n");
 	else
-		return st1.stack[--st1.sp];
+		return st->stack[--st->sp];
 }
