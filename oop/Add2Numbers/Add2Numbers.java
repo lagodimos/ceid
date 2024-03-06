3b224f4bb73e46b014b53688b6e808e680b70352
Giannis Lagodimos <62063308+giann1s@users.noreply.github.com>
2024-03-06 11:13:24 +0200

    Add Add2Numbers and CompareDoubles

---
+++ oop/Add2Numbers/Add2Numbers.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+class Add2Numbers {
+    public static void main(String args[]) {
+        Stack<Double> dStack = new Stack<>();
+
+        Double doubleOne = 3.1415d;
+        Double doubleTwo = 6.2820d;
+
+        dStack.push(doubleOne);
+        dStack.push(doubleTwo);
+
+        dStack.push( dStack.pop() + dStack.pop() );
+
+        System.out.println( dStack.pop() );
+    }
+}
---
+++ oop/CompareDoubles/CompareDoubles.java
@@ -0,0 +1,11 @@
+class CompareDoubles {
+    public static void main(String args[]) {
+
+        Double doubleOne = 3.1415d;
+        Double doubleTwo = 6.2820d;
+
+        System.out.println(Double.compare(doubleOne, doubleTwo));
+
+        System.out.println(doubleOne.compareTo(doubleTwo));
+    }
+}
